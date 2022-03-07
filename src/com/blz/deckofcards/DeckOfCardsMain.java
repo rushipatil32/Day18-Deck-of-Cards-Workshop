ac365c346311df4b99586266243bd9c5fea8522a
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-07 09:00:02 +0530

    Added:Inital game with deck of cards

---
+++ src/com/blz/deckofcards/DeckOfCardsMain.java
@@ -0,0 +1,10 @@
+package com.blz.deckofcards;
+
+public class DeckOfCardsMain {
+	public static void main(String[] args) {
+		DeckOfCards deckOfCardsGame = new DeckOfCards();
+		deckOfCardsGame.welcome();
+		deckOfCardsGame.deckOfCards();
+	}
+}
+
